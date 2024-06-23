8a7d190450ed707c530cc621bdb78278fbc0a1b7
Alex <alex@pehotin.de>
2024-06-23 16:41:58 +0200

    Initial commit

--- src/components/EmptyPage.jsx
+++ src/components/EmptyPage.jsx
@@ -3,7 +3,7 @@ const EmptyPage = () => {
         <>
             <p>Work in progress.</p>
             <p>If you want to participate in discussion about how this app shoult look like or behave, feel free to join: 
-                <a></a>
+                <a href="https://github.com/FaultierSP/civitaiapiclient">GitHub</a>
             </p>
         </>
     );
